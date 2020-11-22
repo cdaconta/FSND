404ec72299fa8485d5090a8d15d3e376027e35ff
Christian Daconta <cdaconta@gmail.com>
2020-11-22 12:11:41 -0500

    Model.py Changes
    Created Event model

--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -43,22 +43,23 @@ class Fighter(db.Model):
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
     age = db.Column(db.Integer)
-    height = db.Column(db.Integer)
-    weight = db.Column(db.Integer)
-    arm_reach = db.Column(db.String)
-    leg_reach = db.Column(db.String)
+    height = db.Column(db.Float)
+    weight = db.Column(db.Float)
+    arm_reach = db.Column(db.Float)
+    leg_reach = db.Column(db.Float)
     sex = db.Column(db.String(1))
     win = db.Column(db.Integer)
     loss = db.Column(db.Integer)
     draw = db.Column(db.Integer)
     division = db.Column(db.Integer)
+    rank = db.Column(db.Integer)
 
     def __repr__(self):
         return f"<Fighter id='{self.id}' first_name='{self.first_name}' last_name='{self.last_name}' age='{self.age}'\
             height='{self.height}' weight='{self.weight}' arm_reach='{self.arm_reach}' leg_reach='{self.leg_reach}' sex='{self.sex}' \
-                win='{self.win}' loss='{self.loss}' draw='{self.draw}' division = '{self.division}'>"
+                win='{self.win}' loss='{self.loss}' draw='{self.draw}' division = '{self.division}' rank='{self.rank}'>"
 
-    def __init__(self, first_name, last_name, age, height, weight, arm_reach, leg_reach, sex, win, loss, draw, division):
+    def __init__(self, first_name, last_name, age, height, weight, arm_reach, leg_reach, sex, win, loss, draw, division, rank):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -71,6 +72,7 @@ class Fighter(db.Model):
         self.loss = loss
         self.draw = draw
         self.division = division
+        self.rank = rank
         
 
     def insert(self):
@@ -99,6 +101,7 @@ class Fighter(db.Model):
             'loss':self.loss,
             'draw':self.draw,
             'division':self.division,
+            'rank':self.rank,
         }
 
 class Division(db.Model):
@@ -117,6 +120,11 @@ class Division(db.Model):
     women_flyweight = db.Column(db.Integer)
     women_bantamweight = db.Column(db.Integer)
     women_featherweight = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<Division id='{self.id}' men_flyweight='{self.men_flyweight}' men_bantamweight='{self.men_bantamweight}' men_featherweight='{self.men_featherweight}'\
+            men_lightweight='{self.men_lightweight}' men_welterweight='{self.men_welterweight}' men_middleweight='{self.men_middleweight}' men_lightheavyweight='{self.men_lightheavyweight}' men_heavyweight='{self.men_heavyweight}' \
+                women_strawweight='{self.women_strawweight}' women_flyweight='{self.women_flyweight}' women_bantamweight='{self.women_bantamweight}'  women_featherweight='{self.women_featherweight}' >"
 
     def __init__(self, men_flyweight, men_bantamweight, men_featherweight, men_lightweight, men_welterweight, 
 men_middleweight, men_lightheavyweight, men_heavyweight, women_strawweight, women_flyweight, women_bantamweight, women_featherweight):
@@ -160,4 +168,53 @@ men_middleweight, men_lightheavyweight, men_heavyweight, women_strawweight, wome
             'women_bantamweight':self.women_bantamweight,
             'women_featherweight':self.women_featherweight,
         }
+
+class Event(db.Model):
+    __tablename__ = 'events'
     
+    id = db.Column(db.Integer, primary_key=True)
+    event_name = db.Column(db.String)
+    event_date = db.Column(db.DateTime) 
+    location = db.Column(db.String)
+    division = db.Column(db.Integer)
+    fighter_1 = db.Column(db.Integer)
+    fighter_2 = db.Column(db.Integer)
+    fighter_1_votes = db.Column(db.Integer, default = 0)
+    fighter_2_votes = db.Column(db.Integer, default = 0)
+
+    def __repr__(self):
+        return f"<Event id='{self.id}' event_name='{self.event_name}' event_date='{self.event_date}' location='{self.location}'\
+            division='{self.division}' weight='{self.weight}' fighter_1='{self.fighter_1}' fighter_2='{self.fighter_2}' fighter_1_votes='{self.fighter_1_votes}' \
+                fighter_2_votes='{self.fighter_2_votes}' >"
+
+    def __init__(self, event_name, event_date, location, division, fighter_1, fighter_2, fighter_1_votes, fighter_2_votes) -> None:
+        self.event_name = event_name
+        self.event_date = event_date
+        self.location = location
+        self.division = division
+        self.fighter_1 = fighter_1
+        self.fighter_2 = fighter_2
+        self.fighter_1_votes = fighter_1_votes
+        self.fighter_2_votes = fighter_2_votes
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'event_name':self.event_name, 
+            'event_date':self.event_date, 
+            'division':self.division,
+            'fighter_1':self.fighter_1,
+            'fighter_2':self.fighter_2,
+            'fighter_1_votes':self.fighter_1_votes,
+            'fighter_2_votes':self.fighter_2_votes,
+        }
